40db924964539d2d67ecb4781e0f4d41f2641220
Robert Lutz <robertlutzi@gmail.com>
2024-01-18 00:07:24 -0500

    implement a non-working sudoku solver that can't recurse properly

--- sudoku.rkt
+++ sudoku.rkt
@@ -31,16 +31,23 @@
 ; =========================
 ; functions
 
-(define (read-square n board)
-  ; [ListOf N] N -> N
-  ; retrieves the value of the square at position n
-  (cond
-    [(= 0 n) (first board)]
-    [else (read-square (sub1 n) (rest board))]))
 
+(define (solve board)
+  (local (
+          (define solns (map (λ (sq) (possibilities sq sudoku)) ITERATOR))
+          (define minimal (n-possible solns))
+          (define square-id (@minimum minimal))
+          (define minsk (list-ref minimal square-id)))
+    ; - IN -
+    (cond
+      [(= 0 minsk) #f]
+      [(solved? board) board]
+      [else
+       (map (λ (n) (try n square-id board)) (list-ref solns square-id))])))
 
+  
 (define (possibilities n board)
-  ; [ListOf N] N -> N
+  ; N [ListOf N] -> N
   ; derive the list of possible values that can naiively solve square n
   (cond
     [(> (read-square n board) 0) #f]
@@ -58,32 +65,48 @@
        (pare impossibilities INTS))]))
 
 
-(define (inpencil board solns)
-  (cond
-    [(empty? solns) '()]
-    [else (local (
-                    (define solvit (first solns)))
-              (cons 
-               (cond
-                 [(false? solvit) (first board)]
-                 [(= 1 (length solvit)) (first solvit)]
-                 [else (first board)])
-               (inpencil (rest board) (rest solns))))]))
-
-
-(define (difficulty n board)
+(define (read-square n board)
   ; [ListOf N] N -> N
-  ; determines the number of possible values that can naiively solve square n
+  ; retrieves the value of the square at position n
   (cond
-    [(> (read-square n board) 0) #f]
-    [else
-     (local (
-             (define squares
-               (foldr append '() (filter (λ (z) (member? n z)) ZONES)))
-             (define impossibilities
-               (list-to-set (map (λ (m) (read-square m board)) squares))))
-       ; - IN -
-       (- 10 (length impossibilities)))]))
+    [(= 0 n) (first board)]
+    [else (read-square (sub1 n) (rest board))]))
+
+
+(define (n-possible maybe-lst)
+  ; [ListOf [Maybe [ListOf N]]] -> N
+  ; determines the length of the shortest remaining solution list
+  (local (
+          (define (len x)
+            (cond
+              [(false? x) 100]
+              [else (length x)])))
+    ; - IN -
+    (map len maybe-lst)))
+
+            
+(define (@minimum lst)
+  ; [ListOf N] -> N
+  ; identifies the index of the first minimum in the list
+  (local (
+          (define (leastest lst mn i n)
+            ; [ListOf N] N N N -> N
+            ; finds mindex by recursion
+            (cond
+              [(empty? lst) i]
+              [(< (first lst) mn) (leastest (rest lst) (first lst) n (add1 n))]
+              [else (leastest (rest lst) mn i (add1 n))])))
+    ; - IN -
+    (leastest lst 10 -1 0)))
+
+
+(define (try num sq board)
+  ; N N [ListOf N] -> [ListOf N]
+  ; tries a solution at the seemingly most solvable square
+  (cond
+    [(empty? board) '()]
+    [(= 0 sq) (cons num (rest board))]
+    [else (cons (first board) (try num (sub1 sq) (rest board)))]))
 
 
 (define (list-to-set lst)
@@ -93,6 +116,10 @@
     [(empty? lst) '()]
     [(member? (first lst) (rest lst)) (list-to-set (rest lst))]
     [else (cons (first lst) (list-to-set (rest lst)))]))
+
+
+(define (solved? board)
+  (not (ormap (λ (s) (= 0)) board)))
 
 
 ; ============================
@@ -107,9 +134,9 @@
                      0 0 8 0 0 0 0 0 2
                      4 0 0 0 3 0 8 7 0
                      0 0 0 7 0 0 0 0 6))
-(define chalunge (map (λ (sq) (possibilities sq sudoku)) ITERATOR))
-chalunge
 
-(define sudoku-1 (inpencil sudoku chalunge))
-(define chalunge-1 (map (λ (sq) (possibilities sq sudoku-1)) ITERATOR))
-chalunge-1
+(define s1 (first (solve sudoku)))
+(define s2 (first (solve s1)))
+(define s3 (first (solve s2)))
+(define s4 (first (solve s3)))
+(map (λ (sq) (possibilities sq s4)) ITERATOR)
